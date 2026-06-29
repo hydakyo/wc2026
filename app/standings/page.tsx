7dd997b80cdf88005e26a1c563268649eb99e346
hydakyo <128904098+hydakyo@users.noreply.github.com>
2026-06-29 11:26:59 +0700

    fix standings text

--- app/standings/page.tsx
+++ app/standings/page.tsx
@@ -3,7 +3,7 @@ import { QualifiedPanel, StandingsGrid, ThirdPlaceRace } from '@/components/Stan
 
 export default function StandingsPage() {
   return (
-    <Shell title="B\u1ea3ng x\u1ebfp h\u1ea1ng" subtitle="To\u00e0n b\u1ed9 b\u1ea3ng \u0111\u1ea5u, nh\u00f3m top 2 \u0111i ti\u1ebfp v\u00e0 cu\u1ed9c \u0111ua top 8 \u0111\u1ed9i h\u1ea1ng ba t\u1ed1t nh\u1ea5t.">
+    <Shell title="Bảng xếp hạng" subtitle="Toàn bộ bảng đấu, nhóm top 2 đi tiếp và cuộc đua top 8 đội hạng ba tốt nhất.">
       <section className="grid two-col"><QualifiedPanel /><ThirdPlaceRace /></section>
       <StandingsGrid />
     </Shell>
