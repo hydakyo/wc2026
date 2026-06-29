43f6d94bb1bafc0917fa4b42ebc7fb163a81fcac
hydakyo <128904098+hydakyo@users.noreply.github.com>
2026-06-29 11:21:17 +0700

    i18n: translate standings page

--- app/standings/page.tsx
+++ app/standings/page.tsx
@@ -3,7 +3,7 @@ import { QualifiedPanel, StandingsGrid, ThirdPlaceRace } from '@/components/Stan
 
 export default function StandingsPage() {
   return (
-    <Shell title="Standings Control" subtitle="Full group tables, top-two qualification view and best third-place race for the 48-team format.">
+    <Shell title="B\u1ea3ng x\u1ebfp h\u1ea1ng" subtitle="To\u00e0n b\u1ed9 b\u1ea3ng \u0111\u1ea5u, nh\u00f3m top 2 \u0111i ti\u1ebfp v\u00e0 cu\u1ed9c \u0111ua top 8 \u0111\u1ed9i h\u1ea1ng ba t\u1ed1t nh\u1ea5t.">
       <section className="grid two-col"><QualifiedPanel /><ThirdPlaceRace /></section>
       <StandingsGrid />
     </Shell>
